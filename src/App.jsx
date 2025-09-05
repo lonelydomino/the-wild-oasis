f13e57883f63bea8af42afad6ebcfa4df899af4b
lonelydomino <lonelydomino@icloud.com>
2025-09-05 00:57:52 -0700

    Update dependencies and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import styled from "styled-components"
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  `
 
 function App() {
 
@@ -5,6 +11,7 @@ function App() {
   return (
     <>
       <div>
+        <H1>The Wild Oasis</H1>
         hello world
       </div>
     </>
