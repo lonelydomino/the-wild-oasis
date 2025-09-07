de80cebcac009f8205a04da052e9b1eba21c55f6
lonelydomino <lonelydomino@icloud.com>
2025-09-06 19:52:30 -0700

    Add GlobalStyles component and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,40 @@ const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   `
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  background-color: purple;
+  color: white;
+  border-radius: 7px;
+  margin: 20px;
+  cursor: pointer;
+`
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding:20px;
+`
+
 
 function App() {
 
 
   return (
     <>
-      <div>
+      <StyledApp>
         <H1>The Wild Oasis</H1>
-        hello world
-      </div>
+
+        <Button>Check In</Button>
+        <Input type='number' placeholder="number of guests"/>
+      </StyledApp>
     </>
   )
 }
