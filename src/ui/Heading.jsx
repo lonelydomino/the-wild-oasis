6f870c6e91068d33cda3a7250a5e7784a898718c
lonelydomino <lonelydomino@icloud.com>
2025-09-08 17:55:44 -0700

    Add Row component and update Heading component

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
-
-
 const Heading = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  `
+  ${(props) =>
+    props.type === "h1" &&
+    `font-size: 30px;
+  font-weight: 600;`}
+  ${(props) =>
+    props.type === "h2" &&
+    `font-size: 2rem;
+  font-weight: 600;`}
+  ${(props) =>
+    props.type === "h3" &&
+    `font-size: 2rem;
+  font-weight: 500;`}
+  line-height: 1.4;
+`;
 
-  export default Heading
+export default Heading;
