e73fb7639729466cf509c37103ee939030fd3c92
lonelydomino <lonelydomino@icloud.com>
2025-09-11 22:55:13 -0700

    Update dependencies and refine App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import styled from "styled-components"
-import GlobalStyles from './styles/GlobalStyles'
-import Button from "./ui/Button"
-import H1 from './ui/Heading'
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding:20px;
-`
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from './pages/Dashboard'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
 
 
 function App() {
@@ -19,13 +14,18 @@ function App() {
 
   return (
     <>
-      <GlobalStyles />
-      <StyledApp>
-        <H1>The Wild Oasis</H1>
-
-        <Button>Check In</Button>
-        <Input type='number' placeholder="number of guests"/>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route path='dashboard' element={<Dashboard />}/>
+          <Route path='bookings' element={<Bookings />}/>
+          <Route path='cabins' element={<Cabins />}/>
+          <Route path='users' element={<Users />}/>
+          <Route path='settings' element={<Settings />}/>
+          <Route path='account' element={<Account />}/>
+          <Route path='login' element={<Login />}/>
+          <Route path='*' element={<PageNotFound />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
